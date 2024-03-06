0c3a30a254d980e96651d81b2955e855dd2bc510
Arturo Díaz Garza <artudiaz1999@gmail.com>
2024-03-06 03:13:51 -0600

    Libraries

--- front_eeg/server.ts
+++ front_eeg/server.ts
@@ -36,8 +36,8 @@ export function app(): express.Express {
         publicPath: browserDistFolder,
         providers: [{ provide: APP_BASE_HREF, useValue: baseUrl }],
       })
-      .then((html) => res.send(html))
-      .catch((err) => next(err));
+      .then((html: string) => res.send(html))
+      .catch((err: any) => next(err));
   });
 
   return server;
