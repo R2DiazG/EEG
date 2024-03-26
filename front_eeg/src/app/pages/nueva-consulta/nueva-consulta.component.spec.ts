ca95ab8e4553a0a2209fa35079ad20cc2c297c37
valentinacruzo <valentina.cruz@udem.edu>
2024-03-26 01:19:38 -0600

    new consult

---
+++ front_eeg/src/app/pages/nueva-consulta/nueva-consulta.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NuevaConsultaComponent } from './nueva-consulta.component';
+
+describe('NuevaConsultaComponent', () => {
+  let component: NuevaConsultaComponent;
+  let fixture: ComponentFixture<NuevaConsultaComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [NuevaConsultaComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(NuevaConsultaComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ front_eeg/src/app/pages/nueva-consulta/nueva-consulta.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nueva-consulta',
+  templateUrl: './nueva-consulta.component.html',
+  styleUrl: './nueva-consulta.component.scss'
+})
+export class NuevaConsultaComponent {
+
+}
