0cf88434f780266a3eff3c925ef4a9897096df60
Arturo Díaz Garza <artudiaz1999@gmail.com>
2024-03-26 03:38:28 -0600

    Models and CRUD
    Completed Models, Update CRUD Usuarios and Pacientes, Eliminated all other CRUDs

--- flask_back_eeg/back_eeg_app/app.py
+++ flask_back_eeg/back_eeg_app/app.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager, create_access_token
-from flask_bcrypt import Bcrypt
+#from flask_bcrypt import Bcrypt # Importar la extensión Bcrypt para encriptar contraseñas de forma segura (hashing)
 from dotenv import load_dotenv
 import os
 
@@ -32,20 +32,25 @@ from models import Usuario, Rol, Genero, EstadoCivil, Escolaridad, Lateralidad, 
 def health():
     return jsonify({'status': 'up'}), 200
 
-# Rutas de la API (ejemplo para registro y autenticación)
-
 ################################################################## CRUD de Usuarios ##################################################################
 @app.route('/usuarios', methods=['POST'])
 def crear_usuario():
     datos = request.get_json()
+    if not datos or 'username' not in datos or 'contraseña' not in datos:
+        return jsonify({'mensaje': 'Datos insuficientes para crear un usuario'}), 400
+    # Verificar la existencia del rol
+    rol = Rol.query.get(datos.get('id_rol'))
+    if not rol:
+        return jsonify({'mensaje': 'Rol no válido'}), 400
+    hashed_password = bcrypt.generate_password_hash(datos['contraseña']).decode('utf-8')
     nuevo_usuario = Usuario(
-        nombre=datos['nombre'],
-        apellidos=datos['apellidos'],
-        username=datos['username'],
-        contraseña=bcrypt.generate_password_hash(datos['contraseña']).decode('utf-8'),
-        correo=datos['correo'],
-        aprobacion=datos['aprobacion'],
-        id_rol=datos['id_rol']
+        nombre=datos.get('nombre'),
+        apellidos=datos.get('apellidos'),
+        username=datos.get('username'),
+        contraseña=hashed_password,
+        correo=datos.get('correo'),
+        aprobacion=datos.get('aprobacion', True),  # Valor predeterminado a True si no se proporciona
+        id_rol=datos.get('id_rol')
     )
     db.session.add(nuevo_usuario)
     db.session.commit()
@@ -54,19 +59,15 @@ def crear_usuario():
 @app.route('/usuarios', methods=['GET'])
 def obtener_usuarios():
     usuarios = Usuario.query.all()
-    resultado = []
-    for usuario in usuarios:
-        usuario_datos = {
-            'id_usuario': usuario.id_usuario,
-            'nombre': usuario.nombre,
-            'apellidos': usuario.apellidos,
-            'username': usuario.username,
-            # No retornar la contraseña
-            'correo': usuario.correo,
-            'aprobacion': usuario.aprobacion,
-            'id_rol': usuario.id_rol
-        }
-        resultado.append(usuario_datos)
+    resultado = [{
+        'id_usuario': usuario.id_usuario,
+        'nombre': usuario.nombre,
+        'apellidos': usuario.apellidos,
+        'username': usuario.username,
+        'correo': usuario.correo,
+        'aprobacion': usuario.aprobacion,
+        'id_rol': usuario.id_rol
+    } for usuario in usuarios]
     return jsonify(resultado), 200
 
 @app.route('/usuarios/<int:id_usuario>', methods=['GET'])
@@ -87,17 +88,18 @@ def obtener_usuario(id_usuario):
 def actualizar_usuario(id_usuario):
     usuario = Usuario.query.get_or_404(id_usuario)
     datos = request.get_json()
-    
+    if 'id_rol' in datos:
+        rol = Rol.query.get(datos['id_rol'])
+        if not rol:
+            return jsonify({'mensaje': 'Rol no válido'}), 400
+        usuario.id_rol = datos['id_rol']
     usuario.nombre = datos.get('nombre', usuario.nombre)
     usuario.apellidos = datos.get('apellidos', usuario.apellidos)
     usuario.username = datos.get('username', usuario.username)
-    # No actualizar la contraseña directamente sin hashing
     if 'contraseña' in datos:
         usuario.contraseña = bcrypt.generate_password_hash(datos['contraseña']).decode('utf-8')
     usuario.correo = datos.get('correo', usuario.correo)
     usuario.aprobacion = datos.get('aprobacion', usuario.aprobacion)
-    usuario.id_rol = datos.get('id_rol', usuario.id_rol)
-
     db.session.commit()
     return jsonify({'mensaje': 'Usuario actualizado exitosamente'}), 200
 
@@ -109,48 +111,173 @@ def eliminar_usuario(id_usuario):
     return jsonify({'mensaje': 'Usuario eliminado exitosamente'}), 200
 ######################################################################################################################################################
 #––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––#
-################################################################### CRUD de Roles ####################################################################
-@app.route('/roles', methods=['POST'])
-def crear_rol():
+################################################################ CRUD de Pacientes ###################################################################
+@app.route('/pacientes', methods=['POST'])
+def crear_paciente():
     datos = request.get_json()
-    nuevo_rol = Rol(rol=datos['rol'], descripcion=datos['descripcion'])
-    db.session.add(nuevo_rol)
+    nuevo_paciente = Paciente(
+        id_usuario=datos['id_usuario'],
+        nombre=datos['nombre'],
+        apellido_paterno=datos['apellido_paterno'],
+        apellido_materno=datos['apellido_materno'],
+        fecha_nacimiento=datos['fecha_nacimiento'],
+        id_genero=datos['id_genero'],
+        id_estado_civil=datos['id_estado_civil'],
+        id_escolaridad=datos['id_escolaridad'],
+        id_lateralidad=datos['id_lateralidad'],
+        id_ocupacion=datos['id_ocupacion']
+    )
+    db.session.add(nuevo_paciente)
+    db.session.flush()  # Para obtener el id del paciente recién creado
+    # Añadir teléfonos, correos electrónicos y direcciones si se proporcionan
+    if 'telefonos' in datos:
+        for num in datos['telefonos']:
+            nuevo_telefono = Telefono(numero=num, id_paciente=nuevo_paciente.id_paciente)
+            db.session.add(nuevo_telefono)
+    if 'correos_electronicos' in datos:
+        for email in datos['correos_electronicos']:
+            nuevo_correo = CorreoElectronico(email=email, id_paciente=nuevo_paciente.id_paciente)
+            db.session.add(nuevo_correo)
+    if 'direcciones' in datos:
+        for direccion in datos['direcciones']:
+            nueva_direccion = Direccion(direccion=direccion, id_paciente=nuevo_paciente.id_paciente)
+            db.session.add(nueva_direccion)
     db.session.commit()
-    return jsonify({'mensaje': 'Rol creado exitosamente'}), 201
+    return jsonify({'mensaje': 'Paciente creado exitosamente'}), 201
 
-@app.route('/roles', methods=['GET'])
-def obtener_roles():
-    roles = Rol.query.all()
-    resultado = [{'id_rol': rol.id_rol, 'rol': rol.rol, 'descripcion': rol.descripcion} for rol in roles]
+@app.route('/pacientes', methods=['GET'])
+def obtener_pacientes():
+    pacientes = Paciente.query.all()
+    resultado = []
+    for paciente in pacientes:
+        # Información básica del paciente
+        paciente_datos = {
+            'id_paciente': paciente.id_paciente,
+            'id_usuario': paciente.id_usuario,
+            'nombre': paciente.nombre,
+            'apellido_paterno': paciente.apellido_paterno,
+            'apellido_materno': paciente.apellido_materno,
+            'fecha_nacimiento': paciente.fecha_nacimiento.strftime('%Y-%m-%d'),
+            'id_genero': paciente.id_genero,
+            'id_estado_civil': paciente.id_estado_civil,
+            'id_escolaridad': paciente.id_escolaridad,
+            'id_lateralidad': paciente.id_lateralidad,
+            'id_ocupacion': paciente.id_ocupacion,
+            # Agregar detalles relacionados
+            'telefonos': [{'id_telefono': tel.id_telefono, 'telefono': tel.telefono} for tel in paciente.telefonos],
+            'correos_electronicos': [{'id_correo': correo.id_correo, 'correo_electronico': correo.correo_electronico} for correo in paciente.correos_electronicos],
+            'direcciones': [{
+                'id_direccion': direccion.id_direccion,
+                'calle_numero': direccion.calle_numero,
+                'colonia': direccion.colonia,
+                'ciudad': direccion.ciudad,
+                'estado': direccion.estado,
+                'pais': direccion.pais,
+                'codigo_postal': direccion.codigo_postal
+            } for direccion in paciente.direcciones]
+        }
+        resultado.append(paciente_datos)
     return jsonify(resultado), 200
 
-@app.route('/roles/<int:id_rol>', methods=['GET'])
-def obtener_rol(id_rol):
-    rol = Rol.query.get_or_404(id_rol)
-    rol_datos = {'id_rol': rol.id_rol, 'rol': rol.rol, 'descripcion': rol.descripcion}
-    return jsonify(rol_datos), 200
-
-@app.route('/roles/<int:id_rol>', methods=['PUT'])
-def actualizar_rol(id_rol):
-    rol = Rol.query.get_or_404(id_rol)
+@app.route('/pacientes/<int:id_paciente>', methods=['PUT'])
+def actualizar_paciente(id_paciente):
+    paciente = Paciente.query.get_or_404(id_paciente)
     datos = request.get_json()
-    rol.rol = datos.get('rol', rol.rol)
-    rol.descripcion = datos.get('descripcion', rol.descripcion)
+    # Actualizar datos básicos del paciente
+    paciente.nombre = datos.get('nombre', paciente.nombre)
+    paciente.apellido_paterno = datos.get('apellido_paterno', paciente.apellido_paterno)
+    paciente.apellido_materno = datos.get('apellido_materno', paciente.apellido_materno)
+    paciente.fecha_nacimiento = datos.get('fecha_nacimiento', paciente.fecha_nacimiento)
+    # Actualizar relaciones basadas en IDs proporcionados
+    paciente.id_genero = datos.get('id_genero', paciente.id_genero)
+    paciente.id_estado_civil = datos.get('id_estado_civil', paciente.id_estado_civil)
+    paciente.id_escolaridad = datos.get('id_escolaridad', paciente.id_escolaridad)
+    paciente.id_lateralidad = datos.get('id_lateralidad', paciente.id_lateralidad)
+    paciente.id_ocupacion = datos.get('id_ocupacion', paciente.id_ocupacion)
+    # Actualizar teléfonos
+    actualizar_telefonos(paciente, datos.get('telefonos', []))
+    # Actualizar correos electrónicos
+    actualizar_correos(paciente, datos.get('correos_electronicos', []))
+    # Actualizar direcciones
+    actualizar_direcciones(paciente, datos.get('direcciones', []))
     db.session.commit()
-    return jsonify({'mensaje': 'Rol actualizado exitosamente'}), 200
+    return jsonify({'mensaje': 'Paciente actualizado exitosamente'}), 200
 
-@app.route('/roles/<int:id_rol>', methods=['DELETE'])
-def eliminar_rol(id_rol):
-    rol = Rol.query.get_or_404(id_rol)
-    db.session.delete(rol)
+def actualizar_telefonos(paciente, telefonos_nuevos):
+    if telefonos_nuevos is not None:
+        # Obtener los teléfonos actuales y mapearlos por id_telefono
+        telefonos_actuales = {tel.id_telefono: tel for tel in paciente.telefonos}
+        for tel_data in telefonos_nuevos:
+            if 'id_telefono' in tel_data and tel_data['id_telefono'] in telefonos_actuales:
+                telefono = telefonos_actuales.pop(tel_data['id_telefono'])
+                telefono.numero = tel_data['numero']
+            else:
+                nuevo_telefono = Telefono(numero=tel_data['numero'], id_paciente=paciente.id_paciente)
+                db.session.add(nuevo_telefono)
+        # Eliminar cualquier teléfono no incluido en la actualización
+        for tel in telefonos_actuales.values():
+            db.session.delete(tel)
+
+def actualizar_correos(paciente, correos_nuevos):
+    if correos_nuevos is not None:
+        correos_actuales = {correo.id_correo: correo for correo in paciente.correos_electronicos}
+        for correo_data in correos_nuevos:
+            if 'id_correo' in correo_data and correo_data['id_correo'] in correos_actuales:
+                correo = correos_actuales.pop(correo_data['id_correo'])
+                correo.correo_electronico = correo_data['correo_electronico']
+            else:
+                nuevo_correo = CorreoElectronico(correo_electronico=correo_data['correo_electronico'], id_paciente=paciente.id_paciente)
+                db.session.add(nuevo_correo)
+        for correo in correos_actuales.values():
+            db.session.delete(correo)
+
+def actualizar_direcciones(paciente, direcciones_nuevas):
+    if direcciones_nuevas is not None:
+        direcciones_actuales = {direccion.id_direccion: direccion for direccion in paciente.direcciones}
+        for direccion_data in direcciones_nuevas:
+            if 'id_direccion' in direccion_data and direccion_data['id_direccion'] in direcciones_actuales:
+                direccion = direcciones_actuales.pop(direccion_data['id_direccion'])
+                # Actualiza los campos de la dirección aquí
+                direccion.calle_numero = direccion_data.get('calle_numero', direccion.calle_numero)
+                direccion.colonia = direccion_data.get('colonia', direccion.colonia)
+                direccion.ciudad = direccion_data.get('ciudad', direccion.ciudad)
+                direccion.estado = direccion_data.get('estado', direccion.estado)
+                direccion.pais = direccion_data.get('pais', direccion.pais)
+                direccion.codigo_postal = direccion_data.get('codigo_postal', direccion.codigo_postal)
+            else:
+                nueva_direccion = Direccion(
+                    calle_numero=direccion_data['calle_numero'],
+                    colonia=direccion_data['colonia'],
+                    ciudad=direccion_data['ciudad'],
+                    estado=direccion_data.get('estado'),
+                    pais=direccion_data.get('pais'),
+                    codigo_postal=direccion_data.get('codigo_postal'),
+                    id_paciente=paciente.id_paciente
+                )
+                db.session.add(nueva_direccion)
+        # Eliminar cualquier dirección no incluida en la actualización
+        for direccion in direcciones_actuales.values():
+            db.session.delete(direccion)
+
+@app.route('/pacientes/<int:id_paciente>', methods=['DELETE'])
+def eliminar_paciente(id_paciente):
+    paciente = Paciente.query.get_or_404(id_paciente)
+    # Eliminar registros relacionados para mantener la integridad referencial
+    Telefono.query.filter_by(id_paciente=id_paciente).delete()
+    CorreoElectronico.query.filter_by(id_paciente=id_paciente).delete()
+    Direccion.query.filter_by(id_paciente=id_paciente).delete()
+    HistorialMedico.query.filter_by(id_paciente=id_paciente).delete()
+    PacienteMedicamento.query.filter_by(id_paciente=id_paciente).delete()
+    DiagnosticoPrevio.query.filter_by(id_paciente=id_paciente).delete()
+    Sesion.query.filter_by(id_paciente=id_paciente).delete()
+    Consentimiento.query.filter_by(id_paciente=id_paciente).delete()
+    RawEEG.query.filter_by(id_paciente=id_paciente).delete()
+    NormalizedEEG.query.filter_by(id_paciente=id_paciente).delete()
+    # Una vez eliminados los registros relacionados, podemos proceder a eliminar el paciente
+    db.session.delete(paciente)
     db.session.commit()
-    return jsonify({'mensaje': 'Rol eliminado exitosamente'}), 200
+    return jsonify({'mensaje': 'Paciente eliminado exitosamente'}), 200
 ######################################################################################################################################################
-#––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––#
-################################################################ CRUD de Géneros #####################################################################
-
-
-# Rutas adicionales para CRUD de otros modelos
 
 # Manejador global de errores
 @app.errorhandler(Exception)
--- flask_back_eeg/back_eeg_app/models.py
+++ flask_back_eeg/back_eeg_app/models.py
@@ -1,4 +1,11 @@
 from app import db
+from datetime import datetime
+
+# Definición de la tabla de asociación
+paciente_medicamento = db.Table('paciente_medicamento',
+    db.Column('id_paciente', db.Integer, db.ForeignKey('pacientes.id_paciente'), primary_key=True),
+    db.Column('id_medicamento', db.Integer, db.ForeignKey('medicamentos.id_medicamento'), primary_key=True)
+)
 
 # Modelo para Roles
 class Rol(db.Model):
@@ -64,6 +71,19 @@ class Paciente(db.Model):
     id_lateralidad = db.Column(db.Integer, db.ForeignKey('lateralidades.id_lateralidad'))
     id_ocupacion = db.Column(db.Integer, db.ForeignKey('ocupaciones.id_ocupacion'))
 
+    # Relaciones adicionales
+    telefonos = db.relationship('Telefono', backref='paciente', lazy=True)
+    correos_electronicos = db.relationship('CorreoElectronico', backref='paciente', lazy=True)
+    direcciones = db.relationship('Direccion', backref='paciente', lazy=True)
+    historiales_medicos = db.relationship('HistorialMedico', backref='paciente', lazy=True)
+    sesiones = db.relationship('Sesion', backref='paciente', lazy=True)
+    consentimientos = db.relationship('Consentimiento', backref='paciente', lazy=True)
+    raw_eegs = db.relationship('RawEEG', backref='paciente', lazy=True)
+    normalized_eegs = db.relationship('NormalizedEEG', backref='paciente', lazy=True)
+    diagnosticos_previos = db.relationship('DiagnosticoPrevio', backref='paciente', lazy=True)
+    # Como PacienteMedicamento es una asociación
+    medicamentos = db.relationship('Medicamento', secondary=paciente_medicamento, backref=db.backref('pacientes', lazy=True))
+
 # Modelo para Teléfonos
 class Telefono(db.Model):
     __tablename__ = 'telefonos'
@@ -88,3 +108,111 @@ class Direccion(db.Model):
     ciudad = db.Column(db.String(255))
     estado = db.Column(db.String(255))
     pais = db.Column(db.String(255))
+    codigo_postal = db.Column(db.String(20))
+
+class HistorialMedico(db.Model):
+    id_historial_medico = db.Column(db.Integer, primary_key=True)
+    id_paciente = db.Column(db.Integer, db.ForeignKey('pacientes.id_paciente'), nullable=False)
+    historial_medico = db.Column(db.Text)
+
+class DiagnosticoPrevio(db.Model):
+    id_diagnostico_previo = db.Column(db.Integer, primary_key=True)
+    id_paciente = db.Column(db.Integer, db.ForeignKey('pacientes.id_paciente'), nullable=False)
+    descripcion = db.Column(db.Text)
+
+class Sesion(db.Model):
+    __tablename__ = 'sesiones'
+    id_sesion = db.Column(db.Integer, primary_key=True)
+    id_paciente = db.Column(db.Integer, db.ForeignKey('pacientes.id_paciente'), nullable=False)
+    fecha_consulta = db.Column(db.Date, nullable=False)
+    resumen_sesion_actual = db.Column(db.Text)
+    notas_psicologo = db.Column(db.Text)
+
+class Consentimiento(db.Model):
+    __tablename__ = 'consentimientos'
+    id_consentimiento = db.Column(db.Integer, primary_key=True)
+    id_paciente = db.Column(db.Integer, db.ForeignKey('pacientes.id_paciente'), nullable=False)
+    consentimiento = db.Column(db.Boolean, nullable=False)
+    fecha_registro = db.Column(db.DateTime, nullable=False)
+
+class Medicamento(db.Model):
+    __tablename__ = 'medicamentos'
+    id_medicamento = db.Column(db.Integer, primary_key=True)
+    nombre_comercial = db.Column(db.String(255))
+    principio_activo = db.Column(db.String(255))
+    presentacion = db.Column(db.String(255))
+
+class MetricaDesempeno(db.Model):
+    __tablename__ = 'metricas_desempeno'
+    id_metrica = db.Column(db.Integer, primary_key=True)
+    descripcion = db.Column(db.String(255), nullable=False)
+    valor = db.Column(db.Decimal(10, 4))
+    fecha_hora_registro = db.Column(db.DateTime, nullable=False)
+    id_usuario = db.Column(db.Integer, db.ForeignKey('usuarios.id_usuario'))
+
+class LogActividadUsuario(db.Model):
+    __tablename__ = 'logs_actividad_usuarios'
+    id_log = db.Column(db.Integer, primary_key=True)
+    id_usuario = db.Column(db.Integer, db.ForeignKey('usuarios.id_usuario'))
+    accion = db.Column(db.Text, nullable=False)
+    fecha_hora_accion = db.Column(db.DateTime, nullable=False)
+
+class ResultadoPrediccion(db.Model):
+    __tablename__ = 'resultados_prediccion'
+    id_resultado_prediccion = db.Column(db.Integer, primary_key=True)
+    id_eeg_procesado = db.Column(db.Integer, db.ForeignKey('normalized_eeg.id_eeg_procesado'))
+    resultado_prediccion = db.Column(db.Text, nullable=False)
+    nivel_confianza = db.Column(db.Decimal(5, 2))
+    fecha_hora_prediccion = db.Column(db.DateTime, nullable=False)
+
+class RawEEG(db.Model):
+    __tablename__ = 'raw_eeg'
+    id_eeg = db.Column(db.Integer, primary_key=True)
+    id_paciente = db.Column(db.Integer, db.ForeignKey('pacientes.id_paciente'), nullable=False)
+    fecha_hora_registro = db.Column(db.DateTime, nullable=False)
+    Fp1 = db.Column(db.Float)
+    F3 = db.Column(db.Float)
+    C3 = db.Column(db.Float)
+    P3 = db.Column(db.Float)
+    O1 = db.Column(db.Float)
+    F7 = db.Column(db.Float)
+    T3 = db.Column(db.Float)
+    T5 = db.Column(db.Float)
+    Fz = db.Column(db.Float)
+    Fp2 = db.Column(db.Float)
+    F4 = db.Column(db.Float)
+    C4 = db.Column(db.Float)
+    P4 = db.Column(db.Float)
+    O2 = db.Column(db.Float)
+    F8 = db.Column(db.Float)
+    T4 = db.Column(db.Float)
+    T6 = db.Column(db.Float)
+    Cz = db.Column(db.Float)
+    Pz = db.Column(db.Float)
+
+class NormalizedEEG(db.Model):
+    __tablename__ = 'normalized_eeg'
+    id_eeg_procesado = db.Column(db.Integer, primary_key=True)
+    id_eeg = db.Column(db.Integer, db.ForeignKey('raw_eeg.id_eeg'), nullable=False)
+    fecha_hora_procesado = db.Column(db.DateTime, nullable=False)
+    pointStart = db.Column(db.Float)
+    pointInterval = db.Column(db.Float)
+    Fp1 = db.Column(db.Float)
+    F3 = db.Column(db.Float)
+    C3 = db.Column(db.Float)
+    P3 = db.Column(db.Float)
+    O1 = db.Column(db.Float)
+    F7 = db.Column(db.Float)
+    T3 = db.Column(db.Float)
+    T5 = db.Column(db.Float)
+    Fz = db.Column(db.Float)
+    Fp2 = db.Column(db.Float)
+    F4 = db.Column(db.Float)
+    C4 = db.Column(db.Float)
+    P4 = db.Column(db.Float)
+    O2 = db.Column(db.Float)
+    F8 = db.Column(db.Float)
+    T4 = db.Column(db.Float)
+    T6 = db.Column(db.Float)
+    Cz = db.Column(db.Float)
+    Pz = db.Column(db.Float)
