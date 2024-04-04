1774bb5fd9d5cf058fb7583185b21ef46f784af3
valentinacruzo <valentina.cruz@udem.edu>
2024-04-03 21:16:51 -0600

    Nueva sesion

--- front_eeg/src/app/pages/eeg-subir-docs/eeg-subir-docs.component.ts
+++ front_eeg/src/app/pages/eeg-subir-docs/eeg-subir-docs.component.ts
@@ -3,6 +3,9 @@ import { EegService } from '../../services/sesiones/eeg.service';
 import { Router } from '@angular/router';
 import { Location } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
+import { MedicamentoService } from '../../services/medicamentos/medicamento.service';
+import { CrearMedicamentoDialogComponent } from '../crear-medicamento-dialog/crear-medicamento-dialog.component'; // Asegúrate de tener la ruta correcta
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-eeg-subir-docs',
@@ -11,11 +14,13 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class EegSubirDocsComponent {
   selectedFile: File | null = null;
-  fecha: string = ''; // Asigna un string vacío como valor inicial
+  //fecha: string = ''; // Asigna un string vacío como valor inicial
   estadoGeneral: string = ''; // Puede ser 'wakefullness' o cualquier otro valor predeterminado
   resumenSesionActual: string = '';
   idPaciente!: number;
   archivo_eeg!: File;
+  medicamentos: any[] = [];
+  seleccionados: number[] = [];
 
   estadosEspecificos: {[key: string]: boolean} = {
     hiperventilacion: false,
@@ -26,12 +31,15 @@ export class EegSubirDocsComponent {
 
   constructor(
     private eegService: EegService, 
+    private medicamentoService: MedicamentoService,
+    public dialog: MatDialog,
     private router: Router, 
     private location: Location, 
     private route: ActivatedRoute
     ) {}
 
   ngOnInit() {
+    //this.cargarMedicamentos();
     console.log('Hola', this.route.params)
     this.route.paramMap.subscribe(params => {
       console.log(params); // Imprime los parámetros de la URL
@@ -44,6 +52,27 @@ export class EegSubirDocsComponent {
       }
     });
   }
+
+  // cargarMedicamentos(): void {
+  //   this.medicamentoService.obtenerMedicamentos().subscribe({
+  //     next: (medicamentos) => {
+  //       this.medicamentos = medicamentos;
+  //     },
+  //     error: (error) => console.error('Error al cargar medicamentos:', error)
+  //   });
+  // }
+
+  // agregarMedicamento(valor: string): void {
+  //   const id_medicamento = Number(valor);
+  //   if (!isNaN(id_medicamento)) {
+  //     // Si id es un número y no es NaN, procede con tu lógica
+  //     if (!this.seleccionados.includes(id_medicamento)) {
+  //       this.seleccionados.push(id_medicamento);
+  //     }
+  //     console.log(this.seleccionados);
+  //   }
+  // }
+  
 
   onFileSelected(event: any): void {
     this.selectedFile = event.target.files[0];
@@ -72,20 +101,21 @@ export class EegSubirDocsComponent {
     this.location.back();
   }
 
+
+
   onUpload(): void {
     if (!this.selectedFile) {
       alert('Por favor, selecciona un archivo para subir.');
       return;
     }
-    // Asegúrate de tener el idPaciente disponible aquí
+  
     if (!this.idPaciente) {
       alert('El ID del paciente no está disponible.');
       return;
     }
   
     const formData = new FormData();
-    formData.append('eegFile', this.selectedFile, this.selectedFile.name);
-    formData.append('fecha', this.fecha);
+    formData.append('archivo_eeg', this.selectedFile, this.selectedFile.name);
     formData.append('estado_general', this.estadoGeneral);
     
     const estadosEspecificosSeleccionados = Object.entries(this.estadosEspecificos)
@@ -94,18 +124,24 @@ export class EegSubirDocsComponent {
     
     formData.append('estado_especifico', estadosEspecificosSeleccionados.join(','));
     formData.append('resumen_sesion_actual', this.resumenSesionActual);
-    formData.append('id_paciente', this.idPaciente.toString()); // Añadir el idPaciente al FormData
+    formData.append('id_paciente', this.idPaciente.toString());
+  
+    // Iterar sobre los valores de FormData y mostrarlos
+    formData.forEach((value, key) => {
+      console.log(key + ':' + value);
+    });
   
     this.eegService.crearNuevaSesion(formData).subscribe({
       next: (response) => {
         console.log(response);
         alert('EEG subido exitosamente.');
-        // Navegar a otro componente o actualizar la vista según sea necesario
+        this.location.back();
       },
       error: (error) => {
         console.error(error);
         alert('Hubo un problema al subir el EEG.');
       }
     });
-  }  
+  }
+  
 }
