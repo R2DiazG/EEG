59c1229eecf83bd344539a80978555e7189d107a
Arturo Díaz Garza <artudiaz1999@gmail.com>
2024-04-04 23:49:53 -0600

    Highchart DataPSD

--- front_eeg/src/app/pages/graficas-paciente/graficas-paciente.component.ts
+++ front_eeg/src/app/pages/graficas-paciente/graficas-paciente.component.ts
@@ -28,6 +28,13 @@ interface EEGData {
   data: number[][];
 }
 
+interface EEGDataPoints {
+  name: string;
+  data: number[];
+  pointStart: number;
+  pointInterval: number;
+}
+
 @Component({
   selector: 'app-graficas-paciente',
   templateUrl: './graficas-paciente.component.html',
@@ -410,20 +417,17 @@ onSesionChange() {
     }
   }
   
+  // Modificar la función cargarDatosEEG para que utilice la nueva interfaz EEGDataPSD
   cargarDatosEEG(): void {
-    console.log('cargarDatosEEG psd')
-    if (this.idSesion) { // Verifica que idSesion no sea null
+    console.log('cargarDatosEEG psd');
+    if (this.idSesion) {
       this.eegService.obtenerEEGPorSesion(this.idSesion).subscribe({
-        next: (response: { normalized_eegs: string | any[]; }) => {
+        next: (response: any) => {
           console.log('Datos EEG:', response);
-          // Asumiendo que quieres utilizar el primer EEG normalizado que encuentres:
           if (response.normalized_eegs && response.normalized_eegs.length > 0) {
-            const primerEEGNormalizado = response.normalized_eegs[0].data_normalized;
-            // Aquí extraemos 'data_psd' del primer EEG normalizado encontrado
-            const dataPSDString = primerEEGNormalizado.data_psd;
+            const dataPSDString = response.normalized_eegs[0].data_psd;
             try {
-              // Convierte la cadena JSON en un objeto/array JavaScript
-              const dataPSD = JSON.parse(dataPSDString);
+              const dataPSD: EEGDataPoints = JSON.parse(dataPSDString);
               console.log('Datos PSD en JSON:', dataPSD);
               this.procesarYMostrarDatosPSD(dataPSD);
             } catch (error) {
@@ -440,13 +444,21 @@ onSesionChange() {
     }
   }
 
-  procesarYMostrarDatosPSD(dataPSD: any): void {
+  procesarYMostrarDatosPSD(dataPSD: EEGDataPoints): void {
     // Hay que asegurarse de que 'dataPSD' está en el formato correcto para Highcharts, por ejemplo, dataPSD podría ser algo así:
     // [
     //   { name: "Canal 1", data: [...], pointStart: frecuenciaInicial, pointInterval: intervaloEntreFrecuencias },
     //   { name: "Canal 2", data: [...], pointStart: frecuenciaInicial, pointInterval: intervaloEntreFrecuencias },
     //   ...
     // ]
+    // Genera la serie de datos para Highcharts
+    const seriesData = dataPSD.data.map((value, index) => {
+      return [
+        dataPSD.pointStart + index * dataPSD.pointInterval, // x
+        value // y
+      ];
+    });
+    // Opciones para Highcharts
     const options: Options = {
       chart: {
         type: 'line',
@@ -491,161 +503,3 @@ onSesionChange() {
     Highcharts.chart('processed', options); // Asegúrate de que 'processed' es el ID de tu contenedor en HTML
   }  
 }
-
-
-  /*
-  activeTab: string = 'contentEEG'; // Default active tab
-
-  constructor(private http: HttpClient, private router: Router) {}
-  
-  patient: InfoPaciente = new InfoPaciente();
-
-  ngOnInit() {
-    // Si quieres cargar los datos de Highcharts al iniciar el componente:
-    if (this.activeTab === 'contentEEG') {
-      this.loadHighcharts();
-      this.cargarDatosEEGDesdeJSON(); // Nueva llamada para cargar datos desde CSV
-    }
-  }
-
-  setActiveTab(tab: string): void {
-    this.activeTab = tab;
-    // Si cambias a la pestaña de EEG, cargas Highcharts
-    if (tab === 'contentEEG') {
-      this.loadHighcharts();
-      this.cargarDatosEEGDesdeJSON(); // Nueva llamada para cargar datos desde CSV
-    }
-  }
-
-  loadHighcharts() {
-    this.http.get('assets/data_for_highcharts.json').subscribe((data: any) => {
-      const options: Options = {
-        chart: {
-          type: 'line',
-          zooming: {
-            type: 'x' // Configuramos el nuevo tipo de zoom
-          },
-          height: 400
-        },
-        title: {
-          text: 'Power Spectrum Density (PSD) of EEG Data'
-        },
-        xAxis: {
-          title: {
-            text: 'Sample Number'
-          }
-        },
-        yAxis: {
-          title: {
-            text: 'Amplitude (µV)'
-          },
-          labels: {
-            format: '{value:.2f}'
-          }
-        },
-        tooltip: {
-          shared: true,
-          valueSuffix: ' µV',
-          valueDecimals: 2
-        },
-        plotOptions: {
-          line: {
-            lineWidth: 1,
-            //crosshairs: true,
-            marker: {
-              enabled: false
-            }
-          }
-        },
-        series: data as Highcharts.SeriesOptionsType[]
-      };
-
-      Highcharts.chart('processed', options);
-    }, error => {
-      console.error('Error loading the Highcharts data: ', error);
-    });
-  }
-
-  private cargarDatosEEGDesdeJSON(): void {
-    this.http.get('assets/EEGDataTransposed.json').subscribe((data: any) => {
-      const series = data;
-      // Utilizamos un valor de offset fijo más pequeño o un factor de escala para reducirlo.
-      // Por ejemplo, si los valores oscilan alrededor de 50 unidades entre cada canal, podrías usar ese valor.
-      const offset = Math.max(...series.map((s: { data: any; }) => Math.max(...s.data))) - Math.min(...series.map((s: { data: any; }) => Math.min(...s.data))); // Este valor debería ser ajustado manualmente para que se ajuste bien a tus datos.
-  
-      const offsetSeries = series.map((s: { name: string; data: number[]; }, i: number) => ({
-        name: s.name,
-        data: s.data.map(d => d + i * offset), // Offset aplicado a los datos
-      }));
-  
-      const options: Options = {
-        chart: {
-          renderTo: 'eeg',
-          zooming: {
-            type: 'x' // Configuramos el nuevo tipo de zoom
-          },
-          type: 'line',
-          height: 800 // Altura ajustada a la cantidad de datos.
-        },
-        boost: {
-          useGPUTranslations: true
-        },
-        title: {
-          text: 'EEG Data Visualization'
-        },
-        xAxis: {
-          title: {
-            text: 'Sample Number'
-          }
-        },
-        yAxis: {
-          title: {
-            text: 'Amplitude'
-          },
-          tickInterval: offset,
-          labels: {
-            formatter: function () {
-              const index = Math.floor((this.value as number) / offset);
-              // Verificamos si el índice está dentro del rango del array de series
-              if (index >= 0 && index < series.length) {
-                return series[index].name;
-              }
-              return '';
-            }
-          }
-        },
-        tooltip: {
-          shared: true
-        },
-        series: offsetSeries as Highcharts.SeriesOptionsType[]
-      };
-  
-      Highcharts.chart(options);
-    }, error => {
-      console.error('Error fetching the JSON data: ', error);
-    });
-  }
-  
-
-  private processEEGData(allText: string): SeriesOptions[] {
-    // Implementación del procesamiento de datos aquí...
-    // Similar a lo descrito previamente
-
-    const processedData: SeriesOptions[] = []; // Declare and initialize the variable
-
-    // Add a return statement to return the processed EEG data
-    return processedData;
-  }
-
-  searchPatient(query: string) {
-    // Implementación existente...
-  }
-
-  uploadEEG() {
-    this.router.navigate(['/eeg-subir-docs']);
-  }
-
-  addConsultation(): void {
-      this.router.navigate(['/nueva-consulta']);
-  }
-}*/
